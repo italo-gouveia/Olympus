d06ed1d39789a08e8a2ceaaf31d7b61725cf8cc5
Italo Gouveia <italo_2512@hotmail.com>
2020-06-25 19:13:00 -0300

    Playing with AOP

--- src/main/java/com/olympus/OlympusStartup.java
+++ src/main/java/com/olympus/OlympusStartup.java
@@ -1,5 +1,9 @@
 package com.olympus;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +16,9 @@ public class OlympusStartup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlympusStartup.class, args);
+	    //Logger logger = LoggerFactory.getLogger(OlympusStartup.class);
+	    //logger.info("[INFO] Method={}, msg={}", "OlympusStartup.Main", "Executed this function");
+	    //logger.error("Timestamp={}, Class={},  msg={}", new Date(), OlympusStartup.class, "Error at execution");
 	}
 
 }
